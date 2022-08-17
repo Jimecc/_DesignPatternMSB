9960da50328acdb002989e44c389cf6770fd4316
Jimecc <1813085460@qq.com>
2022-08-17 18:35:53 +0800

    单例模式 ｜ enum 枚举

---
+++ src/main/java/com/mashibing/dp/1.singleton/Mgr08.java
@@ -0,0 +1,20 @@
+/**
+ * @author Jim
+ * @Description 枚举
+ * @createTime 2022年08月17日
+ */
+public enum Mgr08 {
+
+    INSTANCE;
+
+    public void m() {}
+
+    public static void main(String[] args) {
+        for(int i=0; i<100; i++) {
+            new Thread(()->{
+                System.out.println(Mgr08.INSTANCE.hashCode());
+            }).start();
+        }
+    }
+
+}
