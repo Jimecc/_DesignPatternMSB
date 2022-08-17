ea96edf0dabac1570b19a45220fb6382ba4f92da
Jimecc <1813085460@qq.com>
2022-08-17 18:35:28 +0800

    单例模式 ｜ 懒汉式 双重检查

---
+++ src/main/java/com/mashibing/dp/1.singleton/Mgr06.java
@@ -0,0 +1,40 @@
+/**
+ * @author Jim
+ * @Description 懒汉式 ｜ 双重检查
+ * @createTime 2022年08月17日
+ */
+public class Mgr06 {
+    private static volatile Mgr06 INSTANCE; //JIT
+
+    private Mgr06() {
+    }
+
+    public static Mgr06 getInstance() {
+        if (INSTANCE == null) {
+            //双重检查
+            synchronized (Mgr06.class) {
+                if(INSTANCE == null) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    INSTANCE = new Mgr06();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public void m() {
+        System.out.println("m");
+    }
+
+    public static void main(String[] args) {
+        for(int i=0; i<100; i++) {
+            new Thread(()->{
+                System.out.println(Mgr06.getInstance().hashCode());
+            }).start();
+        }
+    }
+}
