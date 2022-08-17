77e157e6435d0f617c8a1e11d18bf5dc8ad61a35
Jimecc <1813085460@qq.com>
2022-08-17 19:13:29 +0800

    策略模式

---
+++ src/main/java/com/mashibing/dp/2.strategy/Cat.java
@@ -0,0 +1,29 @@
+/**
+ * @author Jim
+ * @Description
+ * @createTime 2022年08月17日
+ */
+public class Cat {
+
+    int weight, height;
+
+    public Cat(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int compareTo(Cat c) {
+
+        if(this.weight < c.weight){ return -1;}
+        else if(this.weight > c.weight){ return 1;}
+        else {return 0;}
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
+}
